33eeae3bcc6392ecc902db2980dd354dde7d2901
Yan Tavares <yantavares2002@gmail.com>
2023-03-23 19:46:11 -0300

    Atualizações no código

--- simulacao_1.py
+++ simulacao_1.py
@@ -1,17 +1,15 @@
-# Importar os módulos necessários
 import numpy as np
 import soundfile as sf
 
-# Ler o arquivo de áudio da resposta ao impulso e extrair apenas o primeiro canal (mono)
+# Lendo o arquivo de áudio da resposta ao impulso e amostra gravada e extraindo apenas o primeiro canal (mono)
 impulse_data, impulse_rate = sf.read("Church_Schellingwoude.wav")
 impulse_data = impulse_data[:, 0]
 
-# Ler o arquivo de áudio da amostra de voz e extrair apenas o primeiro canal (mono)
 voice_data, voice_rate = sf.read("amostraOriginal.wav")
 voice_data = voice_data[:, 0]
 
-# Realizar uma convolução entre a amostra de voz e a resposta ao impulso usando numpy
+# Realizando uma convolução entre a amostra de voz e a resposta ao impulso 
 convolution_data = np.convolve(voice_data, impulse_data)
 
-# Salvar o resultado da convolução em um arquivo de áudio usando soundfile
+# Salvar o resultado da convolução em um arquivo de áudio 
 sf.write("Convolution.wav", convolution_data, voice_rate)
