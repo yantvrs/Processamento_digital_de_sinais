30e9fbeeabd947e0daf06e75336e6becf62a6b89
Yan Tavares <yantavares2002@gmail.com>
2023-03-23 20:15:20 -0300

    Atualizações

--- simulacao_1/simulacao_1.py
+++ simulacao_1/simulacao_1.py
@@ -1,15 +1,17 @@
 import numpy as np
 import soundfile as sf
+# 1. Implemente a Convolução da Soma e a utilize para processar um sinal de voz considerando a resposta ao impulso definida pelo arquivo de áudio ’Church Schellingwoude.wav’.Essa resposta ao impulso caracteriza a distorção acústica causada na voz ao se falar no interior da igreja de Schellingwoude em Amsterdã.
 
-# Lendo o arquivo de áudio da resposta ao impulso e amostra gravada e extraindo apenas o primeiro canal (mono)
+# a) O sistema deve armazenar esse audio em um vetor. O áudio fornecido tem formato estéreo (2 canais). Utilize apenas o primeiro canal do áudio (mono), daqui por diante chamado de resposta ao impulso.
 impulse_data, impulse_rate = sf.read("Church_Schellingwoude.wav")
 impulse_data = impulse_data[:, 0]
 
+# b) Capture uma amostra de sua voz e a armazene em um vetor.
 voice_data, voice_rate = sf.read("amostraOriginal.wav")
 voice_data = voice_data[:, 0]
 
-# Realizando uma convolução entre a amostra de voz e a resposta ao impulso 
+# c) Realize uma convolução entre a sua amostra de voz e a resposta ao impulso.
 convolution_data = np.convolve(voice_data, impulse_data)
 
-# Salvar o resultado da convolução em um arquivo de áudio 
+# d) Salve o resultado da convolução em um arquivo de áudio.
 sf.write("Convolution.wav", convolution_data, voice_rate)
