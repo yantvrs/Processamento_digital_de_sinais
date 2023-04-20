69df37bc37a1450f102d1ba4fca29057a41888b7
Yan Tavares <yantavares2002@gmail.com>
2023-04-19 21:33:53 -0300

    Simulação 2 pds

--- simulacao_2/code.py
+++ simulacao_2/code.py
@@ -1,27 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-# Estima a TFTD
-def dtft(x,n,k):    
-    M=len(k)  # Amostras
-    X=np.zeros((1,M),dtype=np.complex128)
-    # Somatório
-    for i in range(M):
-        X[0,i]=np.sum(x*np.exp(-1j*np.pi/M*np.outer(n.T,k[i])))
+# Função que faz a estimativa da TFTD
+def dtft(x, n, k):
+    M = 150
+    X = np.zeros_like(k,dtype=np.complex128)
+    for i, freq in enumerate(k):
+        X[i] = np.sum(x*np.exp(-1j*(np.pi/M)*n*freq))
     return X
 
-# Definindo a sequência x[n] = (0,9*exp(jπ/3))^n, 0 ≤ n ≤ 10
-n = np.arange(11)
+n = np.arange(0,10)
 x = (0.9*np.exp(1j*np.pi/3))**n
-
-# Calculando TFTD
-k = np.arange(501)
+k = np.arange(-400,401)
 X = dtft(x,n,k)
 
-# Plota a magnitude de X(e^(jw))
+# Plota a magnetude de X(e^(jw))
 plt.subplot(2,1,1)
-plt.plot(k,np.abs(X[0,:]))
+plt.plot(k, np.abs(X))
 plt.title('Magnitude X(e^(jw))')
 plt.xlabel('Frequência (k)')
 plt.ylabel('|X(e^(jw))|')
@@ -29,7 +24,7 @@ plt.grid(True)
 
 # Plota a fase de X(e^(jw))
 plt.subplot(2,1,2)
-plt.plot(k,np.angle(X[0,:]))
+plt.plot(k, np.angle(X))
 plt.title('Fase de X(e^(jw))')
 plt.xlabel('Frequência (k)')
 plt.ylabel('Ângulo X(e^(jw))(rad)')
